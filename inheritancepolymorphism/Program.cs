470107860c8357e3a5fe71cf3645cfcacbd2b7e7
Vivek Vasavada <vivek.vasavada@gmail.com>
2017-07-13 10:13:40 -0400

    initial commit

---
+++ inheritancepolymorphism/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritancepolymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
